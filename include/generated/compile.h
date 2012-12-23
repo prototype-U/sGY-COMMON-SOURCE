22309f2cc869b9868a51cb66af0900de2db02838
hell-lock-xda <naman.arora11@gmail.com>
2012-12-23 18:08:21 +0530

    #47

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 47 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#47 PREEMPT Sun Dec 23 17:56:23 IST 2012"
+#define LINUX_COMPILE_TIME "17:56:23"
+#define LINUX_COMPILE_BY "hell-lock"
+#define LINUX_COMPILE_HOST "helllock-VirtualBox"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
